afd82d7601fad5276e07eed62c62e8f5c6d786a9
184anony <49863944+184anony@users.noreply.github.com>
2023-04-19 10:23:30 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,20 +1,21 @@
+import { TinySegmenter } from "https://code4fukui.github.io/TinySegmenter/TinySegmenter.js";
+
 // The function here takes the parameters that you
 // have declared in the `glide.json` file, in the
 // same order.
-window.function = function (str, start, end) {
+window.function = function (str) {
   // For each parameter, its `.value` contains
   // either its value in the type you've declared,
   // or it's `undefined`.  This is a good place to
   // extract the `.value`s and assign default
   // values.
-  str = str.value ?? "";
-  start = start.value ?? 0;
-  end = end.value;
+
+  const segs = TinySegmenter.segment(str);
 
   // Your function should return the exact type
   // you've declared for the `result` in
   // `glide.json`, or `undefined` if there's an
   // error or no result can be produced, because a
   // required input is `undefined`, for example.
-  return str.substring(start, end);
+  return segs;
 }
