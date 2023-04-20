4be6bc048cba904a1d5b09dd852eae5d969db1f8
184anony <49863944+184anony@users.noreply.github.com>
2023-04-20 11:17:56 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,10 +1,12 @@
 function test(str){
   const DICT_PATH = "./dict";
+  let tokens;
 	
   kuromoji.builder({dicPath: DICT_PATH}).build((err, tokenizer)=>{
-    return tokenizer.tokenize(str);// 解析データの取得
-    
+    tokens = tokenizer.tokenize(str);// 解析データの取得
   });
+	
+  return tokens.toString();
 }
 
 window.function = function (str) {
