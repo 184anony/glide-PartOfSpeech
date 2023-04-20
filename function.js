8111ce2075dfac0fa8210a42320bb9594c922ffc
184anony <49863944+184anony@users.noreply.github.com>
2023-04-20 10:39:51 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,18 +1,10 @@
-// The function here takes the parameters that you
-// have declared in the `glide.json` file, in the
-// same order.
 window.function = function (str) {
-  // For each parameter, its `.value` contains
-  // either its value in the type you've declared,
-  // or it's `undefined`.  This is a good place to
-  // extract the `.value`s and assign default
-  // values.
-  const segs = TinySegmenter.segment(str);
+  
+  const DICT_PATH = "./dict";
 
-  // Your function should return the exact type
-  // you've declared for the `result` in
-  // `glide.json`, or `undefined` if there's an
-  // error or no result can be produced, because a
-  // required input is `undefined`, for example.
-  return segs;
+  kuromoji.builder({dicPath: DICT_PATH}).build((err, tokenizer)=>{
+		const tokens = tokenizer.tokenize(str);// 解析データの取得
+	});
+
+  return tokens;
 }
